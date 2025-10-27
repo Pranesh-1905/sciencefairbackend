614f00cb64d960845e017327845b69ca489155d9
Pranesh-1905 <727723euai089@gamil.com>
2025-10-27 12:46:38 +0530

    final commit

--- src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
+++ src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
+        .allowedOrigins("https://sciencefairbackend.onrender.com", "http://localhost:5173")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         .allowedHeaders("*")
         .maxAge(3600);
